5899b5b30b007cd8d53e7ca5c7dcfb6b3ba3042b
Álvaro Bustos González <alvaro9noviembre@gmail.com>
2022-11-08 10:27:53 +0100

    prueba

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-
+let prueba = 0;
 let validarFormulario = function(e){    
     const parrafo = document.getElementById("warning");
     console.log("entrando en validarFormulario")
@@ -26,44 +26,34 @@ window.onload = function(){
         if(!validarFormulario()){
             e.preventDefault();
         }
+        
     }
 
     btnEnviar.onclick = function(e){
-        
-        //Variable para seleccionar los radios con el nombre tamaño y esten chekeados
-        let radioActivo = document.querySelector('input[name="tamanio"]:checked');
         //Variable para seleccionar los check con el nombre ingredientes
-        let checks = document.querySelectorAll('input[name="ingredientes"]:checked');
+        let checks = document.getElementById("tableIngredientes")
         //Creo una variable que me dirá la cantidad total de checkbox checkeados
         let cont = 0; 
         //Creo variable para seleccionar el p con el id = total
         let pTotal = document.getElementById("total");
-        let total = 0;
-        //Si hay algun radio activo
-        if(radioActivo) {
-            //a total le sumamos el valor del radio
-            total += Number(radioActivo.value);
+        let total = prueba;
         if(checks){
             //Recorremos con un for la lista de los checks marcados, por cada check marcado
             //le sumamos 1 (de 1€) por ingrediente
-            for(let i = 0; i < checks.length; i++){
-                if(checks[i].checked){
-                    cont = cont + 1;
-                }
-            }
+            
             if(cont==0){
                 e.preventDefault();
                 alert('Debes introducir al menos 1 ingrediente, gracias');
+                let precio = pruebaChico();
+                console.log(precio);
+                console.log(pruebaChico2());
+                alert(`El precio total será ${total} €`)
+                return;
             }
             //Su mamos al total la cantidad de checks marcados
             total = total + cont;
         }
             //En caso de que no haya ningun radio marcado lanzamos el mensaje mediante alert
-        }else {
-            e.preventDefault();
-            alert('Debes seleccionar un tamaño y almenos un ingrediente');
-        }
-
         if(nombre.value.trim() == "" || telefono.value.trim() == ""|| direccion.value.trim() == "" || email.value.trim() == ""){
             pTotal.innerHTML = `Debes introducir todos los campos`
         } else {
@@ -73,7 +63,7 @@ window.onload = function(){
         //Por último modificamos el HTML del <p> con id total y le introducimos el total de la pizza
         //Muestro por alert lo mismo que en parrafo
         alert(`El precio total será ${total} €`)
-
+       
         }
     
     const btnReset = document.getElementById("btnReset");
@@ -92,9 +82,6 @@ window.onload = function(){
             }
             enviarDatosTamanios();
         } 
-       
-       
-       // enviarDatosTamanios();
     }
 }
 //AJAX
@@ -116,6 +103,7 @@ function enviarDatosIngredientes(){
     xmlHttp.open('GET', urlDestino + datosIngredientes, true)
     xmlHttp.send(null)
 }
+
 function enviarDatosTamanios(){
     let xmlHttp = new XMLHttpRequest()
     xmlHttp.onreadystatechange= function(){
@@ -141,15 +129,16 @@ function creacionDatosIngredientes(jsonDoc) {
         const checkLabel = document.createElement("label");
         checkLabel.textContent=ing.NOMBRE+` ${ ing.PVP}€`;
         const check = document.createElement("input");
+        check.id= ing.NOMBRE;
         check.type = "checkbox";
         check.name = "ingrediente"
         checkLabel.appendChild(check);
         td.appendChild(checkLabel);    
         table.appendChild(td)
     }
-      divContenedor.appendChild(table);
+    table.id = "tableIngredientes";
+    divContenedor.appendChild(table);
 }
-
 function creacionDatosTamanios(jsonDoc){
     var objetoJson = JSON.parse(jsonDoc)
     let arrayTamanios = objetoJson.PIZZAS.TAMANIOS;
@@ -177,4 +166,65 @@ function creacionDatosTamanios(jsonDoc){
     //checkeo por defecto
     rdChecked.checked = true;
     
+}
+function calculoPrecioIngredientes(jsonDoc){
+    var objetoJson = JSON.parse(jsonDoc)
+    //Convertimos un texto a un objeto JSON
+    let listado = objetoJson.CATALOGO.INGREDIENTES;
+    let total = 0;
+    for(objeto of listado){
+        let elementoActivo = document.querySelector(`input[id="${objeto.NOMBRE}"]:checked`);
+        let precioElemento = objeto.PVP;
+        if(elementoActivo){
+            total = precioElemento + total;
+        }
+    }
+    console.log(total);
+    return total;
+}
+function precioTamanio(jsonDoc){
+    var objetoJson = JSON.parse(jsonDoc)
+    //Convertimos un texto a un objeto JSON
+    let listado =  objetoJson.PIZZAS.TAMANIOS;
+    let elementoActivo = document.querySelector(`input[name=tamanio]:checked`);
+    console.log(elementoActivo);
+    let total = 0;
+    for(objeto of listado){
+        if(objeto.NOMBRE == elementoActivo.id){
+            total = objeto.PVP;
+        }
+    }
+    console.log(total);
+    return total;
+}
+function pruebaChico(){
+    let xmlHttp = new XMLHttpRequest()
+
+    xmlHttp.onreadystatechange= function(){
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                prueba = precioTamanio(this.responseText)
+                console.log(prueba);
+            } else {
+                alert("¡Error en la obtencion de datos!")
+            }
+        }  
+    }
+    xmlHttp.open('GET', urlDestino + datosDimensiones, true)
+    xmlHttp.send(null)
+    
+}
+function pruebaChico2(){
+    let xmlHttp = new XMLHttpRequest()
+    xmlHttp.onreadystatechange= function(){
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                calculoPrecioIngredientes(this.responseText)
+            } else {
+                alert("¡Error en la obtencion de datos!")
+            }
+        }  
+    }
+    xmlHttp.open('GET', urlDestino + datosIngredientes, true)
+    xmlHttp.send(null)
 }
